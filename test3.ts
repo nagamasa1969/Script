9471dbc25fb82b8a0b252c9c6d05c7166c1d719d
nagamasa1969 <nagamasa1969@icloud.com>
2024-12-22 10:20:15 +0900

    tryCatch
    tryCatch

--- test3.ts
+++ test3.ts
@@ -18,25 +18,31 @@ const getUserInfo = (id: number) => {
   } else {
     // エラー
     return {
-      status: Status.Error,
-      errorLog: "User not found",
+      errorLog: "User not found"
     };
   }
 }
 
 //以下別ファイルの考え
-const result = getUserInfo(1);
-// 備考: getUserInfoの型は以下のようになっている
-// type Response = {
-//  status: string;
-//  user?: User;
-//  errorLog?: string;
-// };
-
-if(result.status === Status.Success){
-  // userの型がoptionalなので、コンパイルエラーを防ぐには冗長なチェックが必要。
-  console.log(result.user.name); // Error: result.user is possibly 'undefined'.
-}else{
-  // エラー時にもuserプロパティが存在してしまうため、不適切にアクセス可能。
-  console.log("error:", result.errorLog);
+try {
+    const result = getUserInfo(2);
+    // 備考: getUserInfoの型は以下のようになっている
+    // type Response = {
+    //  status: string;
+    //  user?: User;
+    //  errorLog?: string;
+    // };
+    
+    //if(result.status === Status.Success){
+    //  // userの型がoptionalなので、コンパイルエラーを防ぐには冗長なチェックが必要。
+    //  console.log(result.user.name); // Error: result.user is possibly 'undefined'.
+    //}else{
+    //  // エラー時にもuserプロパティが存在してしまうため、不適切にアクセス可能。
+    //  console.log("error:", result.errorLog);
+    //}
+    //ユーザー名称を取得
+    console.log(result.user.name);
+} catch (error) {
+    //失敗時はエラーを取得、関数の中までは確認できなかった。
+    console.log(error);
 }
