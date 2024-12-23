5c4a490beac1ff27411f0d51ac927cbc8f940ed6
nagamasa1969 <nagamasa1969@icloud.com>
2024-12-23 15:18:50 +0900

    catch部分をリファクタリング
    catch部分をリファクタリング

---
+++ test3_リファクタリング版.ts
@@ -0,0 +1,48 @@
+enum Status {
+  Success = "success",
+  Error = "error",
+}
+
+type User = {
+  id: number;
+  name: string;
+}
+
+const getUserInfo = (id: number) => {
+  if (id === 1) {
+    // 取得成功。
+    return {
+      status: Status.Success,
+      user: { id: 1, name: "Taro" }, // User型
+    };
+  } else {
+    // エラー
+    return {
+      errorLog: "User not found"
+    };
+  }
+}
+
+const i = 2;
+const result = getUserInfo(i);
+try {
+    // 備考: getUserInfoの型は以下のようになっている
+    // type Response = {
+    //  status: string;
+    //  user?: User;
+    //  errorLog?: string;
+    // };
+    
+    //if(result.status === Status.Success){
+    //  // userの型がoptionalなので、コンパイルエラーを防ぐには冗長なチェックが必要。
+    //  console.log(result.user.name); // Error: result.user is possibly 'undefined'.
+    //}else{
+    //  // エラー時にもuserプロパティが存在してしまうため、不適切にアクセス可能。
+    //  console.log("error:", result.errorLog);
+    //}
+    //ユーザー名称を取得
+    console.log(result.user.name);
+} catch (error) {
+    //失敗時はエラーを取得
+    console.log(`Error: ${result.errorLog}`);
+}
